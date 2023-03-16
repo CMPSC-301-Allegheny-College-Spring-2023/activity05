255cca010096f55095c74768d65a34dc0154d60e
Oliver Bonham-Carter <obonhamcarter@allegheny.edu>
2023-03-15 21:00:19 -0400

    init

---
+++ src/code.r
@@ -0,0 +1,52 @@
+### Name
+# Add your name here
+
+# Please answer the following questions below. If you are writing code, then be sure that your code will run with all necessary libraries. All other responses are to be left as comments in the code.
+
+# Clear all variables
+
+rm(list = ls()) # clear out the variables from memory to make a clean execution of the code.
+
+# If you want to remove all previous plots and clear the console, run the following two lines.
+graphics.off() # clear out all plots from previous work.
+
+cat("\014") # clear the console
+
+# Follow the seven steps from the [tutorial](https://www.scribbr.com/statistics/anova-in-r/). Please add the code from each step below. When you are finished, you should be able to run your code without errors appearing. For each step, please provide two sentences in _clear and meaningful language_ to describe what the code is doing.
+
+
+# The seven steps of the tutorial.
+
+# Step 1: Load the data into R
+  # TODO (What is happening during this step?)
+  # TODO (add your code here from the tutorial)
+
+# Step 2: Perform the ANOVA test
+  # TODO (What is happening during this step?)
+  # TODO (add your code here from the tutorial)
+
+# Step 3: Find the best-fit model
+  # TODO (What is happening during this step?)
+  # TODO (add your code here from the tutorial)
+
+# Step 4: Check for homoscedasticity
+  # TODO (What is happening during this step?)
+  # TODO (add your code here from the tutorial)
+
+# Step 5: Do a post-hoc test
+  # TODO (What is happening during this step?)
+  # TODO (add your code here from the tutorial)
+
+# Step 6: Plot the results in a graph
+  # TODO (What is happening during this step?)
+  # TODO (add your code here from the tutorial)
+
+# Step 7: Report the results
+  # TODO (What is happening during this step?)
+  # TODO (add your code here from the tutorial)
+
+
+
+
+
+# (Did you remember to write your name above?)
